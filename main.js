751017539ca80a4ee0c597ea440904ac3b98130e
Muxammad Pulatov <github_mailingiz@example.com>
2026-07-15 17:22:27 +0500

    news

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 let turn = "X";
 
-function choosingFigure(cell){
-    if (cell.classList.contains("band")) return;
-    
+function choosingFigure(cell) {
+    if (cell.classList.contains("busy")) return;
+
     let orasm = cell.children[0];
     let xrasm = cell.children[1];
 
-    if(turn === "X"){
+    if (turn === "X") {
         xrasm.classList.remove("hide");
         xrasm.classList.add("aktiv");
         turn = "O";
-    }else {
-        orasm.classList.remove("hide"); 
+        document.querySelector("h2").innerText = " O ning navbati"
+    } else {
+        orasm.classList.remove("hide");
         orasm.classList.add("aktiv");
         turn = "X";
+        document.querySelector("h2").innerText = " X ning navbati"
     }
     cell.classList.add("band");
 }
+
+
