2b056ce94b670469d62625209d106f30255c29fa
Muxammad Pulatov <github_mailingiz@example.com>
2026-07-24 10:36:18 +0500

    ss

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 let turn = "X";
 let finished = false;
+let options = ["", "", "", "", "", "", "", "", ""];
 
-let options = ["X", "", "", "", "", "", "", "", ""];
-
+const winConditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]             
+];
 
 function choosingFigure(cell, index) {
-    if (finished) return;
-
-    if (cell.classList.contains("busy")) return;
+    if (finished || cell.classList.contains("band")) return;
 
     let orasm = cell.children[0];
     let xrasm = cell.children[1];
@@ -15,48 +17,37 @@ function choosingFigure(cell, index) {
     if (turn === "X") {
         xrasm.classList.remove("hide");
         xrasm.classList.add("aktiv");
+        options[index] = 'X';
         turn = "O";
         document.querySelector("h2").innerText = "O ning navbati";
-        options[index] = 'X';
     } else {
         orasm.classList.remove("hide");
         orasm.classList.add("aktiv");
+        options[index] = 'O';
         turn = "X";
         document.querySelector("h2").innerText = "X ning navbati";
-        options[index] = 'O';
     }
 
     cell.classList.add("band");
-    finished = check();
+
+    let winningLine = check(); 
+
+    if (winningLine) {
+        finished = true;
+        let winner = turn === "X" ? "O" : "X";
+        document.querySelector("h2").innerText = `${winner} g'alaba qozondi!`;
+
+        const cells = document.querySelectorAll('.cell');
+        winningLine.forEach(idx => cells[idx].classList.add('winner-flash'));
+    } else if (!options.includes("")) {
+        finished = true;
+        document.querySelector("h2").innerText = "Durang!";
+    }
 }
 
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
-
-
 function check() {
-    for (var i = 0; i < winConditions.length; i++) {
-        var tmp = "";
-        for (var j = 0; j < winConditions[i].length; j++) {
-            if (options[winConditions[i][j]] == "") {
-                tmp = "";
-                break;
-            } else if (tmp == "") {
-                tmp = options[winConditions[i][j]];
-            } else if (tmp != options[winConditions[i][j]]) {
-                tmp = "";
-                break;
-            }
-        }
-        if (tmp != "") return true;
-    }
-    return false;
+    return winConditions.find(condition => {
+        const [a, b, c] = condition;
+        return options[a] !== "" && options[a] === options[b] && options[a] === options[c];
+    }) || false;
 }
